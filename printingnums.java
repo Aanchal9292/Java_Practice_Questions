0743c707eede680f57f8398d0e0c60f59a4760c9
Aanchal9292 <aanchal9280@gmail.com>
2026-02-19 14:36:05 +0530

    Add files via upload

---
+++ printingnums.java
@@ -0,0 +1,11 @@
+package BasicsOfJava;
+public class printingnums {
+    public static void main(String[] args) {
+        // System.out.println("4");
+        // System.out.println(4);
+        // System.out.println(4+3);
+        // System.out.println("4+3");
+        int x = 4 + 2 % -8;
+        System.out.println(x);
+    }
+}
